f29691eec28ca966ce6b752ac139aa1c3e6920ef
Luca Dall'Olio <luca.dallolio@aego.ai>
2024-03-22 17:44:11 +0100

    adding fortran and some dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,7 @@ fi
 echo "**************** F R E E F E M  B U I L D  S T A R T S  H E R E ****************"
 
 autoreconf -i
+export FFLAGS=-fallow-argument-mismatch
 ## Required to make linker look in correct prefix
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
@@ -16,14 +17,13 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 ./configure --prefix=$PREFIX \
             --enable-optim \
             --enable-debug \
-            ${FF_OPTIONS} \
-            --with-arpack=${PREFIX}/lib/libarpack${SHLIB_EXT} \
-            --disable-fortran
+            ${FF_OPTIONS} #\
+            #--disable-fortran
 
 make -j $CPU_COUNT
 make install
-rm $PREFIX/lib/ff++/${PKG_VERSION}/lib/*.${SHLIB_EXT} || true # to avoid conda DSO errors
-rm $PREFIX/lib/ff++/${PKG_VERSION}/lib/*.a || true # static libraries are not allowed
-make check -j $CPU_COUNT check
+#rm $PREFIX/lib/ff++/${PKG_VERSION}/lib/*.${SHLIB_EXT} || true # to avoid conda DSO errors
+#rm $PREFIX/lib/ff++/${PKG_VERSION}/lib/*.a || true # static libraries are not allowed
+#make check -j $CPU_COUNT check
 
 echo "**************** F R E E F E M  B U I L D  E N D S  H E R E ****************"
